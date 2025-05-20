f1dcdc800b834cf7f2edab040e311cd9f057a86f
PHUC-A2 <phucban297@gmail.com>
2025-05-20 14:21:38 +0700

    cập nhật giao diện

--- src/main/java/com/example/quanlybansach/domain/model/SanPham.java
+++ src/main/java/com/example/quanlybansach/domain/model/SanPham.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,6 +14,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Document(collection = "sanpham")
 public class SanPham {
     @Id
     private String id;
